a4397e7ac00f2ad8f7d8ae17fa6d0d6dfedc3243
Chris Henrick <chrishenrick@gmail.com>
2013-01-02 18:12:15 -0800

    Added extra credit to exercise 21

--- ex21.py
+++ ex21.py
@@ -34,3 +34,16 @@ print "Here is a puzzle."
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 
 print "That becomes: ", what, "Can you do it by hand?"
+
+#extra credit:
+
+print "Dividing IQ: %d by 2, multiplying the result by weight: %d, subtracting that result from \
+height: %d, and then adding the result to age: %d" % (iq, weight, height, age)
+puzzle = (height - ((iq / 2) * weight)) + age
+
+print "Which equals: %d" % puzzle
+
+#the inverse of the above puzzle?
+inverse = divide(iq, multiply(weight, subtract(height, add(age, 2))))
+
+print "So the inverse is:",inverse, "right?"
