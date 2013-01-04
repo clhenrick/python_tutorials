1bd96e29c38d25bc4c1907304161c413a3d34316
Chris Henrick <chrishenrick@gmail.com>
2013-01-03 18:33:22 -0800

    modified ex16.py

--- ex16.py
+++ ex16.py
@@ -47,8 +47,10 @@ print "I'm going to write %r %r %r to the file." % (line1, line2, line3)
 #target.write("\n")
 
 #extra credit: take the above six lines of code and make them one line (hint: use strings, formats, and escapes):
+
 #new = (line1, "\n", line2, "\n", line3, "\n") #not correct.
 #target.write(new) #not correct.
+
 #the next line is correct!
 target.write("%r\n%r\n%r\n" % (line1,line2,line3))
 
