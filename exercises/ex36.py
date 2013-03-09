e38a5326b9c37368fdc5a36f3c44872e3c356c30
Chris Henrick <chrishenrick@gmail.com>
2013-03-08 17:10:01 -0800

    added ex35_mod.py, ex36.py, and pdf map of ex36.py

---
+++ exercises/ex36.py
@@ -0,0 +1,12 @@
+#! usr/bin/env python
+
+# exercise 36 build your own game.
+
+from sys import exit
+
+def end_game(why)
+    """ends the game and tells the user why."""
+    print why, "It sure is rough out there!"
+    exit(0)
+    
+    
